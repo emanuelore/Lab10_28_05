db866584f410728551db570ef72a6d743de2d907
emanuelore <emanuel.ore@tecsup.edu.pe>
2022-05-28 23:50:04 -0500

    listo

---
+++ Lab10_28_05/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace DAEA_LAB10_JE
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Lab10_28_05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DAEA_LAB10_JE.MvcApplication" Language="C#" %>
